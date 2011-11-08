fbdd32c5b5bfe0a26c39d0ea990c16d4d5af132e
Marcus Mo <marcusmo88@gmail.com>
2011-11-08 23:24:18 +0000

    Added page (Documentation) navigation links

--- page.php
+++ page.php
@@ -9,13 +9,34 @@
   
     <?php the_content(); ?>
 
-    <nav>
-      <div><?php next_posts_link('&laquo; Older Entries') ?></div>
-      <div><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+    <?php
+      $pagelist = get_pages('sort_column=menu_order&sort_order=asc');
+      $pages = array();
+      foreach ($pagelist as $page) {
+         $pages[] += $page->ID;
+      }
+
+      $current = array_search($post->ID, $pages);
+      $prevID  = $pages[$current-1];
+      $nextID  = $pages[$current+1];
+    ?>
+
+    <nav id="page-navigation" class="cf">
+      <?php if ( !empty($prevID) ) { ?>
+      <div class="left">
+        <a href="<?php echo get_permalink($prevID); ?>" title="<?php echo get_the_title($prevID); ?>">&laquo; <?php echo get_the_title($prevID); ?></a>
+      </div>
+      <?php }
+      if ( !empty($nextID) ) { ?>
+      <div class="right">
+        <a href="<?php echo get_permalink($nextID); ?>" title="<?php echo get_the_title($nextID); ?>"><?php echo get_the_title($nextID); ?> &raquo;</a>
+      </div>
+      <?php } ?>
     </nav>
+
   </article>
   <?php endwhile; endif; ?>
-  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+  <?php edit_post_link('Edit this entry.', '<div>', '</div>'); ?>
 
 </div>
 
